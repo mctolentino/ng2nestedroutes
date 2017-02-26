c05bddf3f65eb7bb0464b8d743a5009f8450852e
maki <maki.tolentino@voyagerinnovation.com>
2017-02-26 16:12:17 +0800

    Add links in parent module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {Routes, RouterModule} from "@angular/router";
+import {HomeComponent} from './home/home.component';
+import {ProductsComponent, routes as productRoutes} from './products/products.component';
+import {MainComponent} from './products/main/main.component';
+import {InterestComponent} from './products/interest/interest.component';
+import {SportifyComponent} from './products/sportify/sportify.component';
+import {ByIdComponent} from './products/by-id/by-id.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductsComponent, children: productRoutes}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    MainComponent,
+    InterestComponent,
+    SportifyComponent,
+    ByIdComponent,
+    ByIdComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
